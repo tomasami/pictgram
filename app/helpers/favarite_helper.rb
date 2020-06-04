882e80944112f79d785b621618e624b1b76c090d
EC2 Default User <ec2-user@ip-172-31-45-223.ap-northeast-1.compute.internal>
2020-06-04 11:45:15 +0000

    Third commit

---
+++ app/helpers/favarite_helper.rb
@@ -0,0 +1,2 @@
+module FavariteHelper
+end
---
+++ test/controllers/favarite_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavariteControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
